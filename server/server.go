a7e2a8fc059cb887d0f02bb9f2f85e93a37ed8dc
paullen <bitanpaul1@gmail.com>
2019-05-23 21:59:05 +0530

    Batching v3(sequential)

--- server/server.go
+++ server/server.go
@@ -1,13 +1,17 @@
 package server
 
 import (
+	"context"
+	"database/sql"
+	"encoding/gob"
+	"errors"
 	"sort"
 	"time"
-	"errors"
-	"encoding/gob"
-	"database/sql"
+
 	//"github.com/elgris/sqrl"
-	"github.com/srikrsna/flock/pkg"
+
+	"github.com/elgris/sqrl"
+	flock "github.com/srikrsna/flock/pkg"
 	pb "github.com/srikrsna/flock/protos"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -22,14 +26,15 @@ type Logger interface {
 	Printf(string, ...interface{})
 }
 
-// DB ...
-// type DB interface {
-// 	sqrl.ExecerContext
-// }
+//DB ...
+type DB interface {
+	sqrl.ExecerContext
+	BeginTx(ctx context.Context, opts *sql.TxOptions) (*sql.Tx, error)
+}
 
 // Server ....
 type Server struct {
-	DB     *sql.DB
+	DB     DB
 	Logger Logger
 	Tables map[string]flock.Table
 }
@@ -42,9 +47,18 @@ func (s *Server) Flock(ch pb.Flock_FlockServer) error {
 	var next pb.FlockRequest
 	var nextRequest *pb.BatchInsertHead
 	var receivedChunks = make([]*pb.DataStream, 0)
-	var chunks int
+	var chunks int64
 	var streaming = false
 	var endStream = false
+
+	//Implementation for a single user
+	//To iterate over a database wrap the below code in another for loop
+	tx, err := s.DB.BeginTx(ch.Context(), nil)
+	if err != nil {
+		s.Logger.Printf("unable to create transaction: %v", err)
+		return err
+	}
+	defer tx.Rollback()
 
 	for {
 		if err := ch.RecvMsg(&next); err != nil {
@@ -52,43 +66,52 @@ func (s *Server) Flock(ch pb.Flock_FlockServer) error {
 		}
 
 		switch v := next.Value.(type) {
-			case *pb.FlockRequest_Ping:
+		case *pb.FlockRequest_Ping:
+			if err := ch.Send(&pb.FlockResponse{Value: &pb.FlockResponse_Pong{}}); err != nil {
+				s.Logger.Printf("unable to send echo message: %T", err)
+				return err
+			}
+		case *pb.FlockRequest_Batch:
+			if v == nil || v.Batch == nil {
+				return status.Errorf(codes.InvalidArgument, "nil batch request")
+			}
+			switch batch := v.Batch.Value.(type) {
+			case *pb.Batch_Head:
 				if streaming {
-					s.Logger.Printf("Error: Data streaming in progress")
-					return errors.New("Unresolved data stream")
+					s.Logger.Printf("Error: Data streaming in progress. Can't handle new request.")
+					return errors.New("unresolved data stream")
 				}
-				if err := ch.Send(&pb.FlockResponse{Value: &pb.FlockResponse_Pong{}}); err != nil {
-					s.Logger.Printf("unable to send echo message: %T", err)
-					return err
+				nextRequest = batch.Head
+				chunks = int64(batch.Head.Chunks)
+				streaming = true
+			case *pb.Batch_Chunk:
+				receivedChunks = append(receivedChunks, batch.Chunk)
+				if int64(len(receivedChunks)) == chunks && endStream {
+					streaming = false
 				}
-			case *pb.FlockRequest_Batch:
-				if v == nil || v.Batch == nil {
-					return status.Errorf(codes.InvalidArgument, "nil batch request")
-				}
-				switch batch := v.Batch.Value.(type) {
-					case *pb.Batch_Head:
-						if streaming {
-							s.Logger.Printf("Error: Data streaming in progress. Can't handle new request.")
-							return errors.New("unresolved data stream")
-						}
-						nextRequest = batch.Head
-						chunks = int(batch.Head.Chunks) 
-						// TODO : Resolve Overflow
-						streaming = true
-					case *pb.Batch_Chunk:
-						receivedChunks = append(receivedChunks, batch.Chunk)
-						if len(receivedChunks) == chunks && endStream {
-							streaming = false
-						}
-					case *pb.Batch_Tail:
-						endStream = true
-					default:
-						s.Logger.Printf("might be a version mis match unknown message type received: %T", v.Batch.Value)
-						return status.Errorf(codes.Unimplemented, "must be version mismatch unknown message type: %T", v.Batch.Value)
-				}
+			case *pb.Batch_Tail:
+				endStream = true
 			default:
-				s.Logger.Printf("might be a version mis match unknown message type received: %T", next.Value)
-				return status.Errorf(codes.Unimplemented, "must be version mismatch unknown message type: %T", next.Value)
+				s.Logger.Printf("might be a version mis match unknown message type received: %T", v.Batch.Value)
+				return status.Errorf(codes.Unimplemented, "must be version mismatch unknown message type: %T", v.Batch.Value)
+			}
+		case *pb.FlockRequest_End:
+			if streaming || endStream {
+				s.Logger.Printf("End signal sent mid-stream")
+				return errors.New("End request could not be processed")
+			}
+			if err := ch.Send(&pb.FlockResponse{Value: &pb.FlockResponse_Batch{Batch: &pb.BatchInsertResponse{Success: true}}}); err != nil {
+				s.Logger.Printf("unable to send echo message: %T", err)
+				return err
+			}
+			if err := tx.Commit(); err != nil {
+				s.Logger.Printf("could not commit the transaction: %v", err)
+				return err
+			}
+			return nil
+		default:
+			s.Logger.Printf("might be a version mis match unknown message type received: %T", next.Value)
+			return status.Errorf(codes.Unimplemented, "must be version mismatch unknown message type: %T", next.Value)
 		}
 
 		if !streaming && endStream {
@@ -99,7 +122,7 @@ func (s *Server) Flock(ch pb.Flock_FlockServer) error {
 			for _, v := range receivedChunks {
 				data = append(data, v.GetData()...)
 			}
-			res, err := handleBatch(ch.Context(), s.DB, s.Tables, nextRequest, data)
+			res, err := handleBatch(ch.Context(), tx, s.Tables, nextRequest, data)
 			if err != nil {
 				s.Logger.Printf("unable to handle batch insert request: %v", err)
 				return err
@@ -108,9 +131,10 @@ func (s *Server) Flock(ch pb.Flock_FlockServer) error {
 				s.Logger.Printf("unable to send batch insert response: %v", err)
 				return err
 			}
+			chunks = 0
+			receivedChunks = receivedChunks[:0]
 			nextRequest = nil
 			endStream = false
 		}
 	}
 }
-
